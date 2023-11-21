26738851d9928bce9f2c72c45412350fdc4bbfc9
WelBert-dev <wellison.bertelli@hotmail.com>
2023-11-20 22:24:39 -0300

    Add Carrinho Arduino Bluetooth: Funcionalidade Extra: Controlando o carrinho por comandos TEXTOS enviados pelo App Android Dabble para o modulo bluetooth Rs232 Hc05 utilizando o Terminal Module do Dabble (Além do Game Pad como interface Homen-Máquina, o aplicativo Dabble também incluí várias outras funcionalidades para interações com o Arduíno, uma delas é o Terminal):
    	- Adicionado para nos próximos passos realizar bateria de testes unitários enviando comando texto, e recebendo os feedbacks pelo próprio App (SEM A NECESSIDADE DA JANELA SERIAL DO ARDUÍNO IDE PARA MONITORAMENTOS DOS TESTES);
    
    	- Add switch-cases para direcionar o carrinho de acordo com o comando TEXTO do terminal, f == frente, r == ré, d == direita, e == esquerda;
    
    	- Obs: FUNCIONALIDADE SIMPLES, apenas para testar a comunicação entre o aplicativo Dabble e o carrinho por meio do Terminal Module;

--- ADS-2Semestre-2023/testes_unitarios_carrinho_arduino_bluetooth_trabalho_final/cod_font_carrinho_arduino_bluetooth.ino
+++ ADS-2Semestre-2023/testes_unitarios_carrinho_arduino_bluetooth_trabalho_final/cod_font_carrinho_arduino_bluetooth.ino
@@ -1,50 +1,90 @@
 #define CUSTOM_SETTINGS
 #define INCLUDE_GAMEPAD_MODULE
+#define INCLUDE_TERMINAL_MODULE
+
 #include <Dabble.h>
    
 #define INT1 2
 #define INT2 3
 #define INT3 4
 #define INT4 5
+
+
+String Serialdata = "";
+bool dataflag = 0;
    
 void setup()
 {
   Serial.begin(250000);
-  Dabble.begin(9600,7,6);  
+  Dabble.begin(9600,7,6); 
+  
   pinMode(INT1,OUTPUT);
   pinMode(INT2,OUTPUT);
   pinMode(INT3,OUTPUT);                                                                    
   pinMode(INT4,OUTPUT);
-  Stop();
+  stop();
+
 }
    
    
 void loop() {
-   
-   
-Dabble.processInput();          
-   
-if(GamePad.isUpPressed())
-{
- forward(); 
-}
-else if(GamePad.isDownPressed())
-{
- backward(); 
-}
-else if(GamePad.isLeftPressed())
-{
- left(); 
-}
-else if(GamePad.isRightPressed())
-{
- right(); 
-}
-else
-{
- Stop(); 
-}
-   
+  Dabble.processInput();  
+
+  while (Serial.available() != 0) {
+    Serialdata = String(Serialdata + char(Serial.read()));
+    dataflag = 1;
+  }
+
+  if (dataflag == 1){
+    Terminal.print(Serialdata);
+    Serialdata = "";
+    dataflag = 0;
+  } 
+
+  if(Terminal.available()){
+    while (Terminal.available() != 0)
+    {
+      char command = (char) Terminal.read(); 
+
+      switch ((char) command) {
+        case 'f': // FRENTE
+          forward();
+          break;
+        case 't': // TRÁS
+          backward();
+          break;
+        case 'e': // ESQUERDA
+          left();
+          break;
+        case 'd': // DIREITA
+          right();
+          break;
+        default:
+          stop();
+      }
+    }
+  }       
+     
+  if(GamePad.isUpPressed())
+  {
+    forward();
+  }
+  else if(GamePad.isDownPressed())
+  {
+    backward();
+  }
+  else if(GamePad.isLeftPressed())
+  {
+    left();
+  }
+  else if(GamePad.isRightPressed())
+  {
+    right();  
+  }
+  else
+  {  
+   stop(); 
+  }
 }
 
 void forward() {
@@ -75,8 +115,7 @@ void right() {
   digitalWrite(INT4,HIGH);
 }
 
-      
-void Stop() {
+void stop() {
   digitalWrite(INT1,LOW);
   digitalWrite(INT2,LOW);
   digitalWrite(INT3,LOW);
