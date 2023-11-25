57246493f4bca9aaf0bbe0c2764f8cd08c97cc02
WelBert-dev <wellison.bertelli@hotmail.com>
2023-11-24 23:17:01 -0300

    Add bateria de testes unitários para as direções do carrinho, executados quando o Terminal Module do app android Dabble envia um gatilho texto "t" via bluetooth para o arduíno, executando os seguintes casos de teste (cenários):
    	- WHEN forward is error THEN returns BAD;
    	- WHEN backward is error THEN returns BAD;
    	- WHEN left is error THEN returns BAD;
    	- WHEN right is error THEN returns BAD;
    
    	- WHEN forward is successful THEN returns OK;
    	- WHEN backward is successful THEN returns OK;
    	- WHEN left is successful THEN returns OK;
    	- WHEN right is successful THEN returns OK;
    
    - Obs: O Monitoramento sobre os resultados e feedbacks dos testes são realizados pela JANELA SERIAL DO ARDUINO IDE;
    
    - Saída:
    
    	Iniciando bateria de testes...
    	WHEN forward is error THEN returns BAD
    	Finalizou teste ERROR no forward...
    	WHEN backward is error THEN returns BAD
    	Finalizou teste ERROR no backward...
    	WHEN left is error THEN returns BAD
    	Finalizou teste ERROR no left...
    	WHEN right is error THEN returns BAD
    	Finalizou teste ERROR no right...
    	Test bad passed
    	WHEN forward is successful THEN returns OK
    	Finalizou teste SUCCESSFUL no forward...
    	WHEN backward is successful THEN returns OK
    	Finalizou teste SUCCESSFUL no backward...
    	WHEN left is successful THEN returns OK
    	Finalizou teste SUCCESSFUL no left...
    	WHEN right is successful THEN returns OK
    	Finalizou teste SUCCESSFUL no right...
    	Test ok passed
    	Test summary: 2 passed, 0 failed, 0 skipped, out of 2 test(s).
    	Finalizando bateria de testes...

--- ADS-2Semestre-2023/testes_unitarios_carrinho_arduino_bluetooth_trabalho_final/cod_font_carrinho_arduino_bluetooth.ino
+++ ADS-2Semestre-2023/testes_unitarios_carrinho_arduino_bluetooth_trabalho_final/cod_font_carrinho_arduino_bluetooth.ino
@@ -1,3 +1,9 @@
+// Dependencia para dos testes unitários, 
+// passo-a-passo no último commit ou indo aqui: 
+// https://github.com/mmurdoch/arduinounit
+#include <ArduinoUnit.h>
+
+
 #define CUSTOM_SETTINGS
 #define INCLUDE_GAMEPAD_MODULE
 #define INCLUDE_TERMINAL_MODULE
@@ -8,6 +14,9 @@
 #define INT2 3
 #define INT3 4
 #define INT4 5
+
+int count = 0;
+int qtde_execution_all_tests_cases = 1;
 
 
 String Serialdata = "";
@@ -47,17 +56,8 @@ void loop() {
       char command = (char) Terminal.read(); 
 
       switch ((char) command) {
-        case 'f': // FRENTE
-          forward();
-          break;
-        case 't': // TRÁS
-          backward();
-          break;
-        case 'e': // ESQUERDA
-          left();
-          break;
-        case 'd': // DIREITA
-          right();
+        case 't': // Execute All Test
+          run_all_unit_test();
           break;
         default:
           stop();
@@ -120,4 +120,125 @@ void stop() {
   digitalWrite(INT2,LOW);
   digitalWrite(INT3,LOW);
   digitalWrite(INT4,LOW);  
+}
+
+
+void run_all_unit_test() {
+  while (count <= qtde_execution_all_tests_cases){
+
+    Serial.print(F("Teste Número: "+String(count+1)));  
+    Serial.print(F("Iniciando bateria de testes..."));  
+
+    Test::run();
+
+    Serial.print(F("Finalizando bateria de testes..."));  
+    count++;
+  }
+  Serial.print(F("Generating Summary..."));  
+
+  count = 0;
+}
+
+// Units Tests cases for LEFT FUNCTION (PARA ESQUERDA) HARDWARE LEVEL 
+test(ok) {
+  // Para FRENTE em cenário de sucesso
+  Serial.print(F("- WHEN forward is successful THEN returns OK"));
+  forward();
+  assertEqual(digitalRead(INT1), HIGH);
+  assertEqual(digitalRead(INT2), HIGH);
+  assertEqual(digitalRead(INT3), LOW);
+  assertEqual(digitalRead(INT4), LOW);
+  Serial.print(F("- Finalizou teste SUCCESSFUL no forward..."));
+  delay(800); 
+
+  // Para TRÁS em cenário de sucesso
+  Serial.print(F("- WHEN backward is successful THEN returns OK"));
+  backward();
+  assertEqual(digitalRead(INT1), LOW);
+  assertEqual(digitalRead(INT2), LOW);
+  assertEqual(digitalRead(INT3), HIGH);
+  assertEqual(digitalRead(INT4), HIGH);
+  Serial.print(F("- Finalizou teste SUCCESSFUL no backward..."));
+  delay(800); 
+
+  // Para ESQUERDA em cenário de sucesso
+  Serial.print(F("- WHEN left is successful THEN returns OK"));
+  left();
+  assertEqual(digitalRead(INT1), LOW);
+  assertEqual(digitalRead(INT2), HIGH);
+  assertEqual(digitalRead(INT3), HIGH);
+  assertEqual(digitalRead(INT4), LOW);
+  Serial.print(F("- Finalizou teste SUCCESSFUL no left..."));
+  delay(800); 
+
+  // Para DIREITA em cenário de sucesso
+  Serial.print(F("- WHEN right is successful THEN returns OK"));
+  right();
+  assertEqual(digitalRead(INT1), HIGH);
+  assertEqual(digitalRead(INT2), LOW);
+  assertEqual(digitalRead(INT3), LOW);
+  assertEqual(digitalRead(INT4), HIGH);
+  Serial.print(F("- Finalizou teste SUCCESSFUL no right..."));
+  delay(800); 
+
+  stop();
+}
+
+test(bad) {
+  // Para FRENTE, não pode ser igual a nenhuma outras direções
+  Serial.print(F("- WHEN forward is error THEN returns BAD"));
+  forward();
+  // Não pode ser igual aos setters para trás
+  assertNotEqual(digitalRead(INT1), LOW);
+  assertNotEqual(digitalRead(INT2), LOW);
+  assertNotEqual(digitalRead(INT3), HIGH);
+  assertNotEqual(digitalRead(INT4), HIGH);
+
+  // Também Não pode ser igual aos setters para esquerda e direita, mas TESTAR ESSES CASOS estava sobrecarregando o arduino...
+  Serial.print(F("- Finalizou teste ERROR no forward...")); 
+  delay(800); 
+
+  // =========================================================
+  // Para TRÁS, não pode ser igual a nenhuma outras direções
+  Serial.print(F("- WHEN backward is error THEN returns BAD"));  
+  backward();
+  // Não pode ser igual aos setters para frente
+  assertNotEqual(digitalRead(INT1), HIGH);
+  assertNotEqual(digitalRead(INT2), HIGH);
+  assertNotEqual(digitalRead(INT3), LOW);
+  assertNotEqual(digitalRead(INT4), LOW);
+
+  // Também Não pode ser igual aos setters para esquerda e direita, mas TESTAR ESSES CASOS estava sobrecarregando o arduino...
+  Serial.print(F("- Finalizou teste ERROR no backward..."));
+  delay(800); 
+
+  // =========================================================
+  // Para ESQUERDA, não pode ser igual a nenhuma outras direções
+  Serial.print(F("- WHEN left is error THEN returns BAD")); 
+  left();
+  // Não pode ser igual aos setters para direita
+  assertNotEqual(digitalRead(INT1), HIGH);
+  assertNotEqual(digitalRead(INT2), LOW);
+  assertNotEqual(digitalRead(INT3), LOW);
+  assertNotEqual(digitalRead(INT4), HIGH);
+
+  // Também Não pode ser igual aos setters para frente e trás, mas TESTAR ESSES CASOS estava sobrecarregando o arduino...
+  Serial.print(F("- Finalizou teste ERROR no left...")); 
+  delay(800);
+
+  // =========================================================
+  // Para DIREITA, não pode ser igual a nenhuma outras direções
+  Serial.print(F("- WHEN right is error THEN returns BAD")); 
+  right();
+  // Não pode ser igual aos setters para esquerda
+  assertNotEqual(digitalRead(INT1), LOW);
+  assertNotEqual(digitalRead(INT2), HIGH);
+  assertNotEqual(digitalRead(INT3), HIGH);
+  assertNotEqual(digitalRead(INT4), LOW);
+
+  // Também Não pode ser igual aos setters para frente e para trás, mas TESTAR ESSES CASOS estava sobrecarregando o arduino...
+  Serial.print(F("- Finalizou teste ERROR no right...")); 
+  delay(800);
+
+  stop();
 }
